9e5b05a392be3e209742b635c7598ff42a18242c
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-08-12 16:07:29 +1200

    working through integration with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { socket } from './socket'
+import { ConnectionState } from './components/ConnectionState'
+import { ConnectionManager } from './components/ConnectionManager'
+import { MyForm } from './components/MyForm'
 
-function App() {
+export default function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [fooEvents, setFooEvents] = useState([])
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true)
+    }
+
+    function onDisconnect() {
+      setIsConnected(false)
+    }
+
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value])
+    }
+
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+    socket.on('foo', onFooEvent)
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+      socket.off('foo', onFooEvent)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConnectionState isConnected={isConnected} />
+      <Events events={fooEvents} />
+      <ConnectionManager />
+      <MyForm />
     </div>
-  );
+  )
 }
-
-export default App;
