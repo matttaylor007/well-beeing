3750a8a8d58de277f767acf58f82e89fe96f3078
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-08-13 12:01:52 +1200

    Add question page and css and add route un App

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import ChatPage from './components/ChatPage'
-import socketIO from 'socket.io-client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ChatPage from "./components/ChatPage";
+import socketIO from "socket.io-client";
+import Questionnaire from "./components/Questionnaire";
 
-const socket = socketIO.connect('http://localhost:4000')
+const socket = socketIO.connect("http://localhost:4000");
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
           <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/questionnaire" element={<Questionnaire />}></Route>
+          <Route path="/resources"></Route>
           <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
